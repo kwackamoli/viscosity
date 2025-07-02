90806d168ff2070eb87ab5e9679a17cd7549438d
AliceRHancox <20046@bayfield-high.school.nz>
2025-07-03 10:52:11 +1200

    buttoms might work gang

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private string level1Scene;
+    public void Play()
+    {
+        SceneManager.LoadScene(level1Scene);
+    }
+
+    public void Quit()
+    {
+        #if UNITY_STANDALONE
+                Application.Quit();
+        #endif
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
