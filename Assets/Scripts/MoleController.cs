a2f37234199ea8b6ab65d3a5f72f620f3ffc3ec7
AliceRHancox <20046@bayfield-high.school.nz>
2025-07-02 17:24:22 +1200

    bulgaria stufufuf

--- Assets/Scripts/MoleController.cs
+++ Assets/Scripts/MoleController.cs
@@ -2,6 +2,19 @@ using UnityEngine;
 
 public class MoleController : MonoBehaviour
 {
+    [SerializeField]
+    private KeyCode walk_left = KeyCode.A;
+
+    [SerializeField]
+    private KeyCode walk_right = KeyCode.D;
+
+    [SerializeField]
+    private Rigidbody2D rigidbody;
+
+    private float movementforce = 0;
+
+    [SerializeField]
+    private float movementspeed = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +24,20 @@ public class MoleController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movementforce = 0;
+        if (Input.GetKey(walk_left))
+        {
+            movementforce += -movementspeed;
+        }
+        else if (Input.GetKey(walk_right))
+        {
+            movementforce += movementspeed;
+        }
+        else
+        {
+            movementforce = 0;
+        }
+
+        rigidbody.linearVelocity = new Vector2(movementforce,0);
     }
 }
