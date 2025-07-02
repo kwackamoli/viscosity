929493b11234a0087dea269377a15f25eada58f7
AliceRHancox <20046@bayfield-high.school.nz>
2025-07-03 09:31:25 +1200

    can no longer jump midair

--- Assets/Scripts/MoleController.cs
+++ Assets/Scripts/MoleController.cs
@@ -3,18 +3,32 @@ using UnityEngine;
 public class MoleController : MonoBehaviour
 {
     [SerializeField]
-    private KeyCode walk_left = KeyCode.A;
+    private KeyCode walkLeft = KeyCode.A;
 
     [SerializeField]
-    private KeyCode walk_right = KeyCode.D;
+    private KeyCode walkRight = KeyCode.D;
+
+    [SerializeField]
+    private KeyCode jumpKey1 = KeyCode.W;
+
+    [SerializeField]
+    private KeyCode jumpKey2 = KeyCode.Space;
 
     [SerializeField]
     private Rigidbody2D rigidbody;
 
-    private float movementforce = 0;
+    private float movementForce = 0;
 
     [SerializeField]
-    private float movementspeed = 10f;
+    private float movementSpeed = 10f;
+
+    [SerializeField]
+    private float jumpForce = 10f;
+
+    [SerializeField]
+    private BoxCollider2D groundTrigger;
+
+    private bool isGrounded;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,20 +38,41 @@ public class MoleController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movementforce = 0;
-        if (Input.GetKey(walk_left))
+        movementForce = 0;
+        if (Input.GetKey(walkLeft))
         {
-            movementforce += -movementspeed;
+            movementForce += -movementSpeed;
         }
-        else if (Input.GetKey(walk_right))
+        else if (Input.GetKey(walkRight))
         {
-            movementforce += movementspeed;
+            movementForce += movementSpeed;
         }
         else
         {
-            movementforce = 0;
+            movementForce = 0;
         }
 
-        rigidbody.linearVelocity = new Vector2(movementforce,0);
+        rigidbody.linearVelocity = new Vector2(movementForce,rigidbody.linearVelocityY);
+
+        if (Input.GetKey(jumpKey1) || Input.GetKey(jumpKey2)  && isGrounded) 
+        {
+            rigidbody.linearVelocity = new Vector2(rigidbody.linearVelocityX, jumpForce);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
